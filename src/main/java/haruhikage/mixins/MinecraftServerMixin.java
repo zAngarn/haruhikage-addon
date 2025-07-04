2e31adb664199dcf8829d7759806e5bd6cc2563e
methalox <hola@chat.com>
2025-07-04 22:39:28 +0200

    Change targets in MinecraftServerMixin.java. for better precision

--- src/main/java/haruhikage/mixins/MinecraftServerMixin.java
+++ src/main/java/haruhikage/mixins/MinecraftServerMixin.java
@@ -10,18 +10,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
-    @Inject(method = "tickWorlds", at = @At("TAIL"))
+    @Inject(method = "tickWorlds()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Utils;run(Ljava/util/concurrent/FutureTask;Lorg/apache/logging/log4j/Logger;)Ljava/lang/Object;", ordinal = 0))
     private void populationLoggeStart(CallbackInfo ci) {
         if(HaruhikageAddonSettings.logCertainTickPhases) {
             HaruhikageAddonSettings.LOGGER.info("Player phase has just started. Global Timer: {}", System.nanoTime());
         }
     }
 
-    @Inject(method = "tickWorlds", at = @At("TAIL"))
+    @Inject(method = "tickWorlds()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 0))
     private void populationLoggerEnd(CallbackInfo ci) {
         if(HaruhikageAddonSettings.logCertainTickPhases) {
             HaruhikageAddonSettings.LOGGER.info("Exiting player phase. Global timer: {}", System.nanoTime());
         }
     }
-
 }
